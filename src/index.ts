0a63849ef027e7aafd9935bcefae834a6e984877
Shuang <xingshuang_cool@163.com>
2020-12-08 14:05:15 +0800

    fix: 修改index的外部暴露;

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-export * from "./core/dataTypeEm"
-export * from "./core/dataUnit"
-export * from "./core/hexParse"
-export * from "./utils/hexUtils"
-export * from "./exceptions/hexParseError"
+import * as DataType from "./core/dataTypeEm"
+import DataUnit from "./core/dataUnit"
+import HexParse from "./core/hexParse"
+import * as HexUtils from "./utils/hexUtils"
+import HexParseError from "./exceptions/hexParseError"
 export const author = 'oscura'
+export {
+  DataType,
+  DataUnit,
+  HexParse,
+  HexUtils,
+  HexParseError
+}
--- test/hello.js
+++
@@ -1,5 +0,0 @@
-// import * as t from '../dist/index.js'
-var t = require('../dist/lib/index.js')
-
-console.log(t.add(1, 2))
-console.log(t.tmp)
