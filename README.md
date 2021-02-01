4b30382c28dd8e69ee3feb940e21b3323db71d3e
Shuang <xingshuang_cool@163.com>
2021-02-01 10:16:13 +0800

    test: 修改测试文件和readme文件;

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # IOT-DATA-PARSE
 
-![npm-v1.0.6](https://img.shields.io/badge/npm-v1.0.6-brightgreen)
+![npm-v1.0.7](https://img.shields.io/badge/npm-v1.0.7-brightgreen)
 
 ## CopyRight
 
@@ -55,6 +55,7 @@ At last, the data we need is "01 7E 5F C3 3B 42", when we get it, we can parse i
 # Instance
 
 ```
+/*********************************** EXAMPLE1 ***********************************/
 import { DataUnit, DataType, HexParse, HexUtils } from '@oscura/iot-data-parse'
 
 // data configuration that comes from xls file
@@ -72,7 +73,48 @@ const json = [
 ]
 const dataUnitArray = DataUnit.batchInit(json)
 
-/*********************************** EXAMPLE1 ***********************************/
+// io data source
+const ioStr = '07000000994200999A425CF5C340429C28F5C28F5C40434147AE147AE141A1000042033E776A61636B736F6EE4BB8AE5A4A9E5A4A9E6B094E5A5BD'
+
+// create hex parse (in Uint8Array)
+const hexParse = new HexParse(HexUtils.toHexArray(ioStr))
+
+// parse data
+const newData = hexParse.parseDataArray(dataUnitArray)
+console.log(DataUnit.toArrayString(newData))
+
+// result
+  运行状态: true
+  报警状态: true
+  自动状态: true
+  生产数量: 153 个
+  电流: 32.150001525878906 A
+  电压: 55.2400016784668 V
+  温度: 37.22 ℃, 38.51 ℃
+  频率: 20.125 Hz, 32.81100082397461 Hz
+  用户名: jackson
+  报警内容: 今天天气好
+```
+
+```
+/*********************************** EXAMPLE2 ***********************************/
+import { DataUnit, DataType, HexParse, HexUtils } from '@oscura/iot-data-parse'
+
+// data configuration that comes from xls file
+const json = [
+  { description: "运行状态", unit: "", name: "runStatus", byteOffset: 0, bitOffset: 0, count: 1, dataType: "bool", littleEndian: false },
+  { description: "报警状态", unit: "", name: "alarmStatus", byteOffset: 0, bitOffset: 1, count: 1, dataType: "bool", littleEndian: false },
+  { description: "自动状态", unit: "", name: "autoStatus", byteOffset: 0, bitOffset: 2, count: 1, dataType: "bool", littleEndian: false },
+  { description: "生产数量", unit: "个", name: "productNumber", byteOffset: 1, bitOffset: 0, count: 1, dataType: "int", littleEndian: false },
+  { description: "电流", unit: "A", name: "current", byteOffset: 5, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+  { description: "电压", unit: "V", name: "voltage", byteOffset: 9, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+  { description: "温度", unit: "℃", name: "temperature", byteOffset: 13, bitOffset: 0, count: 2, dataType: "double", littleEndian: false },
+  { description: "频率", unit: "Hz", name: "frequency", byteOffset: 29, bitOffset: 0, count: 2, dataType: "float", littleEndian: false },
+  { description: "用户名", unit: "", name: "username", byteOffset: 37, bitOffset: 0, count: 7, dataType: "string", littleEndian: false },
+  { description: "报警内容", unit: "", name: "alarmContent", byteOffset: 44, bitOffset: 0, count: 20, dataType: "string", littleEndian: false }
+]
+const dataUnitArray = DataUnit.batchInit(json)
+
 // create hex parse (in Uint8Array)
 const hexParse = new HexParse()
 hexParse.addUint8(0x07)
@@ -96,21 +138,7 @@ hexParse.addUint8(0x07)
   const newData = hexParse.parseDataArray(dataUnitArray)
   console.log(DataUnit.toArrayString(newData))
 
-/*********************************** EXAMPLE2 ***********************************/
-// io data source
-const ioStr = '07000000994200999A425CF5C340429C28F5C28F5C40434147AE147AE141A1000042033E776A61636B736F6EE4BB8AE5A4A9E5A4A9E6B094E5A5BD'
-
-// create hex parse (in Uint8Array)
-const hexParse2 = new HexParse(HexUtils.toHexArray(ioStr))
-
-// parse data
-const newData2 = hexParse2.parseDataArray(dataUnitArray)
-console.log(DataUnit.toArrayString(newData2))
-```
-
-result：
-
-```
+// result
   运行状态: true
   报警状态: true
   自动状态: true
--- test/example/example2.test.ts
+++ test/example/example2.test.ts
@@ -2,28 +2,50 @@ import { DataUnit, HexParse, HexUtils } from '../../src/index'
 
 const right = "正确情况"
 
-const json = [
-  { description: "运行状态", unit: "", name: "runStatus", byteOffset: 0, bitOffset: 0, count: 1, dataType: "bool", littleEndian: false },
-  { description: "报警状态", unit: "", name: "alarmStatus", byteOffset: 0, bitOffset: 1, count: 1, dataType: "bool", littleEndian: false },
-  { description: "自动状态", unit: "", name: "autoStatus", byteOffset: 0, bitOffset: 2, count: 1, dataType: "bool", littleEndian: false },
-  { description: "生产数量", unit: "个", name: "productNumber", byteOffset: 1, bitOffset: 0, count: 1, dataType: "int", littleEndian: false },
-  { description: "电流", unit: "A", name: "current", byteOffset: 5, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
-  { description: "电压", unit: "V", name: "voltage", byteOffset: 9, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
-  { description: "温度", unit: "℃", name: "temperature", byteOffset: 13, bitOffset: 0, count: 2, dataType: "double", littleEndian: false },
-  { description: "频率", unit: "Hz", name: "frequency", byteOffset: 29, bitOffset: 0, count: 2, dataType: "float", littleEndian: false },
-  { description: "用户名", unit: "", name: "username", byteOffset: 37, bitOffset: 0, count: 7, dataType: "string", littleEndian: false },
-  { description: "报警内容", unit: "", name: "alarmContent", byteOffset: 44, bitOffset: 0, count: 20, dataType: "string", littleEndian: false }
-]
-const dataUnitArray = DataUnit.batchInit(json)
-
 test(`${right}：example1`, () => {
+  // data configuration that comes from xls file
+  const json = [
+    { description: "运行状态", unit: "", name: "runStatus", byteOffset: 0, bitOffset: 0, count: 1, dataType: "bool", littleEndian: false },
+    { description: "报警状态", unit: "", name: "alarmStatus", byteOffset: 0, bitOffset: 1, count: 1, dataType: "bool", littleEndian: false },
+    { description: "自动状态", unit: "", name: "autoStatus", byteOffset: 0, bitOffset: 2, count: 1, dataType: "bool", littleEndian: false },
+    { description: "生产数量", unit: "个", name: "productNumber", byteOffset: 1, bitOffset: 0, count: 1, dataType: "int", littleEndian: false },
+    { description: "电流", unit: "A", name: "current", byteOffset: 5, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+    { description: "电压", unit: "V", name: "voltage", byteOffset: 9, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+    { description: "温度", unit: "℃", name: "temperature", byteOffset: 13, bitOffset: 0, count: 2, dataType: "double", littleEndian: false },
+    { description: "频率", unit: "Hz", name: "frequency", byteOffset: 29, bitOffset: 0, count: 2, dataType: "float", littleEndian: false },
+    { description: "用户名", unit: "", name: "username", byteOffset: 37, bitOffset: 0, count: 7, dataType: "string", littleEndian: false },
+    { description: "报警内容", unit: "", name: "alarmContent", byteOffset: 44, bitOffset: 0, count: 20, dataType: "string", littleEndian: false }
+  ]
+  const dataUnitArray = DataUnit.batchInit(json)
+
+  // io data source
   const ioStr = '07000000994200999A425CF5C340429C28F5C28F5C40434147AE147AE141A1000042033E776A61636B736F6EE4BB8AE5A4A9E5A4A9E6B094E5A5BD'
+
+  // create hex parse (in Uint8Array)
   const hexParse = new HexParse(HexUtils.toHexArray(ioStr))
+
+  // parse data
   const newData = hexParse.parseDataArray(dataUnitArray)
   console.log(DataUnit.toArrayString(newData))
 })
 
 test(`${right}：example2`, () => {
+  // data configuration that comes from xls file
+  const json = [
+    { description: "运行状态", unit: "", name: "runStatus", byteOffset: 0, bitOffset: 0, count: 1, dataType: "bool", littleEndian: false },
+    { description: "报警状态", unit: "", name: "alarmStatus", byteOffset: 0, bitOffset: 1, count: 1, dataType: "bool", littleEndian: false },
+    { description: "自动状态", unit: "", name: "autoStatus", byteOffset: 0, bitOffset: 2, count: 1, dataType: "bool", littleEndian: false },
+    { description: "生产数量", unit: "个", name: "productNumber", byteOffset: 1, bitOffset: 0, count: 1, dataType: "int", littleEndian: false },
+    { description: "电流", unit: "A", name: "current", byteOffset: 5, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+    { description: "电压", unit: "V", name: "voltage", byteOffset: 9, bitOffset: 0, count: 1, dataType: "float", littleEndian: false },
+    { description: "温度", unit: "℃", name: "temperature", byteOffset: 13, bitOffset: 0, count: 2, dataType: "double", littleEndian: false },
+    { description: "频率", unit: "Hz", name: "frequency", byteOffset: 29, bitOffset: 0, count: 2, dataType: "float", littleEndian: false },
+    { description: "用户名", unit: "", name: "username", byteOffset: 37, bitOffset: 0, count: 7, dataType: "string", littleEndian: false },
+    { description: "报警内容", unit: "", name: "alarmContent", byteOffset: 44, bitOffset: 0, count: 20, dataType: "string", littleEndian: false }
+  ]
+  const dataUnitArray = DataUnit.batchInit(json)
+
+  // create hex parse (in Uint8Array)
   const hexParse = new HexParse()
   hexParse.addUint8(0x07)
     // productNumber
@@ -41,6 +63,8 @@ test(`${right}：example2`, () => {
     // alarmContent
     .addString("今天天气好")
     .assignRdDataViewByAddResult()
+
+  // parse data
   const newData = hexParse.parseDataArray(dataUnitArray)
   console.log(DataUnit.toArrayString(newData))
 })
